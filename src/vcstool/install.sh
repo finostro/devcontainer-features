b2948fa2da58c7fed55a7bf95a261c1ea46bc651
Felipe Inostroza <felipe.inostroza@amtc.uchile.cl>
2025-08-13 17:12:13 -0400

    added vcstool

---
+++ src/vcstool/install.sh
@@ -0,0 +1,9 @@
+apt_get_update() {
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+apt_get_update
+apt-get install -y python3-vcstool
